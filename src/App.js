50f0ed9c97064637fa4a57c1f31e3fe3758683cc
pabentse <pabentse@ifi.uio.no>
2023-06-11 12:31:57 +0200

    apsidf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-//App.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
@@ -15,49 +14,54 @@ function App() {
   const [input6, setInput6] = useState('');
   const [output3, setOutput3] = useState('');
 
-  const handleCalculate1 = (e) => {
-    e.preventDefault();
-    const result = (input1 / 100) * input2;
-    setOutput1(result.toFixed(2));
-  }
+  useEffect(() => {
+    if(input1 && input2 && !isNaN(input1) && !isNaN(input2)) {
+      const result = (input1 / 100) * input2;
+      setOutput1(result.toFixed(2));
+    } else {
+      setOutput1('');
+    }
+  }, [input1, input2]);
 
-  const handleCalculate2 = (e) => {
-    e.preventDefault();
-    const result = (input3 / input4) * 100;
-    setOutput2(result.toFixed(2));
-  }
-  
-  const handleCalculate3 = (e) => {
-    e.preventDefault();
-    const result = ((input6 - input5) / input5) * 100;
-    setOutput3(result.toFixed(2));
-  }
+  useEffect(() => {
+    if(input3 && input4 && !isNaN(input3) && !isNaN(input4)) {
+      const result = (input3 / input4) * 100;
+      setOutput2(result.toFixed(2));
+    } else {
+      setOutput2('');
+    }
+  }, [input3, input4]);
 
-  // JSX code in App.js
-return (
-  <main className="calculator">
-      <form className="form" onSubmit={handleCalculate1}>
+  useEffect(() => {
+    if(input5 && input6 && !isNaN(input5) && !isNaN(input6)) {
+      const result = ((input6 - input5) / input5) * 100;
+      setOutput3(result.toFixed(2));
+    } else {
+      setOutput3('');
+    }
+  }, [input5, input6]);
+
+  return (
+    <main className="calculator">
+      <div className="form">
         What is 
         <input className="calculator-input" value={input1} onChange={e => setInput1(e.target.value)} /> % of 
         <input className="calculator-input" value={input2} onChange={e => setInput2(e.target.value)} />?
-        <button className="calculator-button" data-testid="calculate1-button" type="submit">Calculate</button>
-        <div className="results">{output1}</div>
-      </form>
-    <form className="form" onSubmit={handleCalculate2}>
-      <input className="calculator-input" value={input3} onChange={e => setInput3(e.target.value)} /> is what percentage of 
-      <input className="calculator-input" value={input4} onChange={e => setInput4(e.target.value)} />?
-      <button className="calculator-button" data-testid="calculate2-button" type="submit">Calculate</button>
-      <div className="results">{output2}</div>
-    </form>
-    <form className="form" onSubmit={handleCalculate3}>
-      What is the percentage increase/decrease from 
-      <input className="calculator-input" value={input5} onChange={e => setInput5(e.target.value)} /> to 
-      <input className="calculator-input" value={input6} onChange={e => setInput6(e.target.value)} />?
-      <button className="calculator-button" data-testid="calculate3-button" type="submit">Calculate</button>
-      <div className="results">{output3}</div>
-    </form>
-  </main>
-);
+        <div className="results" style={{color: 'blue'}}>{output1}</div>
+      </div>
+      <div className="form">
+        <input className="calculator-input" value={input3} onChange={e => setInput3(e.target.value)} /> is what percentage of 
+        <input className="calculator-input" value={input4} onChange={e => setInput4(e.target.value)} />?
+        <div className="results" style={{color: 'blue'}}>{output2}</div>
+      </div>
+      <div className="form">
+        What is the percentage increase/decrease from 
+        <input className="calculator-input" value={input5} onChange={e => setInput5(e.target.value)} /> to 
+        <input className="calculator-input" value={input6} onChange={e => setInput6(e.target.value)} />?
+        <div className="results" style={{color: 'blue'}}>{output3}</div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
